1f148a9a13814042c5754d4fd293779b6e01934c
Ryan Simpson <simpson939@byui.edu>
2023-04-17 17:08:26 -0600

    Finished Prep 2 assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string percentage = Console.ReadLine();
+        int grade = int.Parse(percentage);
+        string letterGrade = "NULL";
+
+        if (grade >= 90)
+        {
+            letterGrade = "A";
+        }
+
+        else if (grade >= 80)
+        {
+            letterGrade = "B";
+        }
+
+        else if (grade >= 70)
+        {
+            letterGrade = "C";
+        }
+
+        else if ( grade >= 60)
+        {
+            letterGrade = "D";
+        }
+
+        else
+        {
+            letterGrade = "F";
+        }
+
+        Console.Write($"Your grade is a {letterGrade}");
     }
 }

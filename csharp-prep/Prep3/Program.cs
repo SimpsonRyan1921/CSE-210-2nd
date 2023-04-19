2b9b10d65b7b797d031c9c0a43cb2df10fc27333
Ryan Simpson <simpson939@byui.edu>
2023-04-19 16:25:40 -0600

    Prep 3 Assignment Finished

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,65 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int guesses = 1;
+
+        int gameOn = 1;
+
+        while( gameOn == 1)
+        {
+            Random randomGenerator = new Random();
+            int magicNum =randomGenerator.Next(1, 50);
+            
+            int answer = 0;
+
+            Console.WriteLine("This is a radnom number generator. Try and guess the number from 1-50.");
+            Console.WriteLine("What is your guess? ");
+            string userGuess = Console.ReadLine();
+            answer = int.Parse(userGuess);
+
+            while (answer != magicNum)
+            {
+                if (answer > magicNum)
+                {
+                    Console.WriteLine("Lower");
+                }
+
+                else if (answer < magicNum)
+                {
+                    Console.WriteLine("Higher");
+                }
+
+                else
+                {
+                    Console.WriteLine("Something went wrong");
+                }
+
+                guesses += 1;
+
+                Console.WriteLine("Give it another shot! What's you're guess? ");
+                userGuess = Console.ReadLine();
+                answer = int.Parse(userGuess);             
+
+            }
+
+            Console.WriteLine($"You got the number! It took you {guesses} tries.");
+            
+            Console.WriteLine("Would you like to play again? ");
+            string playOn = Console.ReadLine();
+            if (playOn == "Yes")
+            {
+                gameOn = 1;
+            }
+            
+            else if (playOn == "No" )
+            {
+                gameOn = 0;
+                Console.WriteLine("Thank you for playing! Goodbye.");
+            }
+            else
+            {
+                gameOn = 0;
+            }
+        }
     }
 }

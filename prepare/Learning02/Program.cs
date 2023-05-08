f12c850ef9c49416265e0bf7818c5a2e45894189
Ryan Simpson <simpson939@byui.edu>
2023-05-08 14:08:19 -0600

    Resume assignment

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -2,19 +2,29 @@ using System;
 
 using System.Collections.Generic;
 
-
 namespace Learning02
 {
     public class Program
     {
         static void Main(string[] args)
         {  
-            Console.WriteLine("Hello Learning02 World!");
-            List <int> numList = new List<int>();
-            numList.Add(121);
-            numList.Add(2021);
-            numList.Remove(0);
-             
+            Job job1 = new Job();
+            job1._jobTitle = "Utility";
+            job1._company = "Showpro";
+            job1._startYear = 2018;
+            job1._endYear = 2022;
+
+            Job job2 = new Job();
+            job2._jobTitle = "Lifeguard";
+            job2._company = "Blueray";
+            job2._startYear = 2016;
+            job2._endYear = 2018;
+
+            Resume myResume = new Resume();
+            myResume._name = "Ryan Simpson";
+
+            myResume._jobs.Add(job1);
+            myResume._jobs.Add(job2);
         }
     }
 }
